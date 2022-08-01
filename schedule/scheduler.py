7eafd7ba068790d3c79033773aec5debc66ce40b
matthew vander hoff <m.vanderhoff94@gmail.com>
2022-07-31 22:36:46 -0400

    adding scheduling stuff

--- schedule/scheduler.py
+++ schedule/scheduler.py
@@ -22,20 +22,24 @@ def initialize_week():
         db.week.insert_one({day: week[day]})
 
 
+def get_class_week(class_name:str):
+    this_class = db.classes.find_one({"name":class_name})
+    return this_class['week']
+
 # differnt ways to schedule
 # have a target start and end date and evenly distribute tasks
 # have a start and ideal task density then calculate how long it will take
 
 
-def schedule_class_by_task_weight(start_date:str="TODAY"):
+def schedule_class_by_task_weight(class_name:str, start_date:str="TODAY"):
     if start_date == "TODAY":
         start_date = datetime.date.today()
     else:
         start_date = datetime.date.fromisoformat(start_date)
     current_day = 0
     current_weight = 0
-    class_tasks = collection.find({'subject': 'MS Physical Science'})
-    lw = list(week)
+    class_tasks = collection.find({'subject': class_name})
+    lw = get_class_week(class_name)
     while True:
         try:
             task = class_tasks.next()
@@ -56,10 +60,34 @@ def schedule_class_by_task_weight(start_date:str="TODAY"):
         except:
             break
 
-def schedule_class_by_target_date(start_date_string: str, target_date_string: str):
+def normalize_week(class_week):
+    total_tasks = sum(map((lambda x: int(x['max_tasks'])),class_week))
+    normalized_week = list(map((lambda x: float(x['max_tasks'])/total_tasks),class_week))
+    return normalized_week
+
+def schedule_class_by_target_date(class_name: str, start_date_string: str, target_date_string: str):
     start_date = datetime.date.fromisoformat(start_date_string)
     target_date = datetime.date.fromisoformat(target_date_string)
-    pass
+    class_week = get_class_week(class_name)
+    # since we cant just use the values in the schedule we get an idea of the
+    # workload wanted on each day by dividing the max tasks per day against
+    # the sum of the weeks max tasks
+    normalized_week = normalize_week(class_week)
+    # we then get the number of days we want to finish in
+    schedule_time_delta = target_date - start_date
+    number_of_days = schedule_time_delta.days
+    # then for each day we look up the normalized task weight for and add
+    # it to a total so we can distribute the tasks to the weighted days
+    # of the week
+    normalized_task_total = 0.0
+    for x in range(number_of_days):
+        normalized_task_total += normalized_week[x%7]
+    class_tasks = collection.find({'$and':[{'subject': class_name},{'completed':False}]})
+    num_tasks = class_tasks.retrieved
+
+
+
+
 
 
 def get_this_weeks_schedule():
