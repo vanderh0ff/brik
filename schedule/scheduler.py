b378bebc0be810aedc77a05a1497ce6c308cd280
matthew vander hoff <m.vanderhoff94@gmail.com>
2022-07-31 20:50:41 -0400

    update multiple schedule methods

--- schedule/scheduler.py
+++ schedule/scheduler.py
@@ -6,58 +6,65 @@ import datetime
 # see how many tasks each day can have
 # get start date
 
- 
 
 def initialize_week():
     week = {
-        'monday': {'max_tasks':12},
-        'tuesday': {'max_tasks':12},
-        'wednesday': {'max_tasks':12},
-        'thursday': {'max_tasks':12},
-        'friday': {'max_tasks':12},
-        'saturday': {'max_tasks':12},
-        'sunday': {'max_tasks':12}
-        }
+        'monday': {'max_tasks': 12},
+        'tuesday': {'max_tasks': 12},
+        'wednesday': {'max_tasks': 12},
+        'thursday': {'max_tasks': 12},
+        'friday': {'max_tasks': 12},
+        'saturday': {'max_tasks': 12},
+        'sunday': {'max_tasks': 12}
+    }
     # get the db connection
     for day in week:
-        db.week.insert_one({day : week[day]})
-    
+        db.week.insert_one({day: week[day]})
 
 
 # differnt ways to schedule
-# have a target start and end date and evenly distribute tasks 
+# have a target start and end date and evenly distribute tasks
 # have a start and ideal task density then calculate how long it will take
 
 
-def schedule_class():
-    today = datetime.date.today()
+def schedule_class_by_task_weight(start_date:str="TODAY"):
+    if start_date == "TODAY":
+        start_date = datetime.date.today()
+    else:
+        start_date = datetime.date.fromisoformat(start_date)
     current_day = 0
-    daily_tasks = []
     current_weight = 0
-    class_tasks = collection.find({'subject':'MS Physical Science'})
+    class_tasks = collection.find({'subject': 'MS Physical Science'})
     lw = list(week)
     while True:
         try:
             task = class_tasks.next()
-            daily_tasks.append(task)
             current_weight += task['weight']
-            if current_weight > week[lw[current_day%7]]['max_tasks']:
+            if current_weight > week[lw[current_day % 7]]['max_tasks']:
                 current_day += 1
-                current_weight = 0
-            daily_tasks[-1]['scheduled_for'] = datetime.datetime.combine(
-                today + datetime.timedelta(days=current_day),
-                datetime.time.min
-                )
+                current_weight = task['weight']
+            query = {'_id': task['_id']}
+            update = {"$set":
+                      {'scheduled_for': datetime.datetime.combine(
+                          start_date + datetime.timedelta(
+                              days=current_day
+                          ),
+                          datetime.time.min)
+                       }
+                      }
+            db.tasks.update_one(query, update)
         except:
             break
-    for task in daily_tasks:
-        query = {'_id':task['_id']}
-        update = {"$set":{'scheduled_for':datetime.datetime.combine(task['scheduled_for'],datetime.time.min)}}
-        db.tasks.update_one(query,update)
+
+def schedule_class_by_target_date(start_date_string: str, target_date_string: str):
+    start_date = datetime.date.fromisoformat(start_date_string)
+    target_date = datetime.date.fromisoformat(target_date_string)
+    pass
+
 
 def get_this_weeks_schedule():
     query = {
-        'scheduled_for': { 
+        'scheduled_for': {
             '$lt': datetime.datetime.today() + datetime.timedelta(days=7)
         }
     }
