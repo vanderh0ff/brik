e4c195a2d83a89fb8e00d1e4d8684ed9fc96b44b
Matthew Vander Hoff <m.vanderhoff94@gmail.com>
2022-08-21 01:41:18 -0400

    add start of ui

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+from data.db import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-        return "<p>Hello, World!</p>"
+        return render_template('index.html', comments=db.getCollectionNames())
