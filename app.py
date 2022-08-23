b3fa13c2011574b8fcd590a82e5a1b47c962868c
Matthew Vander Hoff <m.vanderhoff94@gmail.com>
2022-08-22 20:41:39 -0400

    adding class ui and new test data

--- app.py
+++ app.py
@@ -6,3 +6,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
         return render_template('index.html', comments=db.list_collection_names())
+
+@app.route("/classes")
+def show_classes():
+        classes = db.tasks.distinct('subject')
+        return render_template("classes.html",classes=classes)
+
+@app.route("/classes/<class_name>")
+def show_class(class_name):
+        query = {
+                "subject": class_name
+        }
+        this_class_tasks = db.tasks.find(query)
+        return render_template('show_class.html', class_name=class_name, tasks=this_class_tasks)
